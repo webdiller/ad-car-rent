b3d8f750e4dc2a085ad931d5dbaf39917872541a
Eugene Butkov <eugenefromrus@gmail.com>
2023-09-05 23:10:52 +1000

    feat: upd footer

--- src/widgets/Footer/index.tsx
+++ src/widgets/Footer/index.tsx
@@ -1,7 +1,7 @@
-"use client"
+"use client";
 
-import Link from "next/link"
-import React, { FC } from "react"
+import Link from "next/link";
+import React, { FC } from "react";
 
 interface ComponentProps {}
 
@@ -9,16 +9,14 @@ const Footer: FC<ComponentProps> = () => {
   return (
     <footer className="mt-auto bg-zinc-900 text-white">
       <div className="border-b-[1px] border-[#bfa37c]/20"></div>
-      <div className="container flex items-center justify-between py-4">
-        <Link
-          className="italic tracking-wider sm:text-xl"
-          href="/">
+      <div className="container flex flex-col justify-between py-4 sm:flex-row sm:items-center">
+        <Link className="italic tracking-wider sm:text-xl" href="/">
           A.D. Car Rent
         </Link>
         Копирайт © {new Date().getFullYear()} {process.env.SITE_NAME}. Все права защищены
       </div>
     </footer>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
