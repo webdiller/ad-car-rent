c7a073d228340be322f6db665fd1d0a5b52b2d0b
Eugene Butkov <eugenefromrus@gmail.com>
2023-09-07 21:15:57 +1000

    feat: add next-images

--- next.config.js
+++ next.config.js
@@ -1,7 +1,18 @@
-/** @type {import('next').NextConfig} */
+const withImages = require("next-images");
 const nextTranslate = require("next-translate-plugin");
-const nextConfig = {
-  reactStrictMode: true,
-};
+/** @type {import('next').NextConfig} */
+module.exports = (phase, { defaultConfig }) => {
+  const imagesConfig = withImages(defaultConfig);
+  const translateConfig = nextTranslate(defaultConfig);
 
-module.exports = nextTranslate(nextConfig);
+  // Merge the configurations of both plugins
+  const mergedConfig = {
+    ...imagesConfig,
+    ...translateConfig,
+    reactStrictMode: true,
+    // Add any additional configurations or overrides here
+    // For example, you can customize the Webpack configuration or other Next.js settings.
+  };
+
+  return mergedConfig;
+};
--- src/widgets/SaveMoney/index.tsx
+++ src/widgets/SaveMoney/index.tsx
@@ -1,6 +1,7 @@
 import { contacts } from "@/shared/contacts";
 import { removePlusAndSpaces } from "@/shared/helpers/removePlusAndSpaces";
 import useTranslation from "next-translate/useTranslation";
+import bgImage from "@/public/images/welcome-bg.jpg"
 
 const SaveMoney = () => {
   const { t } = useTranslation("saveMoney");
@@ -8,7 +9,7 @@ const SaveMoney = () => {
   return (
     <div className="relative h-[60vh] bg-black">
       <div className="absolute inset-0 h-full w-full">
-        <img className="h-full w-full object-cover" src="/images/welcome-bg.jpg" alt="" />
+        <img className="h-full w-full object-cover" src={bgImage.src} alt="" />
       </div>
       <div className="absolute inset-0 flex h-full w-full flex-col items-start justify-end bg-black/50 text-white">
         <div className="container flex flex-col items-center space-y-4 rounded-md bg-black/20 p-6 text-center">
